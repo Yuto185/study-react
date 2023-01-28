460d85449991e55714d52eec73947ac1c6354dd5
Yuto Kitagawa <fhvhaw@gmail.com>
2023-01-28 11:11:54 +0900

    export default の削除

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import {Footer} from "../components/Footer";
 import {Links} from "../components/Links"
-import Headline from '../components/Headline';
+import {Headline} from '../components/Headline';
 
-export default function Home() {
+export function Home() {
   return (
     <div className={styles.container}>
       <Head>
