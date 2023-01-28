3e26bcc441133fbc2375a66f91461345fe1119db
Yuto Kitagawa <fhvhaw@gmail.com>
2023-01-28 13:23:01 +0900

    スタイルファイルの分割

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css';
+import classes from '../styles/Home.module.css';
 import {Footer} from "../components/Footer";
 import { Main } from '../components/Main';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={classes.container}>
       <Head>
         <title>Index Page</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,39 +13,6 @@ export default function Home() {
 
       <Main page="index" />
       <Footer />
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
